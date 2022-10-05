61138a5c38af522382c5e2dbe9e50d88d0cf768b
Wonkeun No <zakk23v@hotmail.com>
2022-10-05 19:35:13 -0400

    Implement sending a fragment to microservice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import TextField from '@mui/material/TextField';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import Avatar from '@mui/material/Avatar';
-import IconButton from '@mui/material/IconButton';
+import { useState } from 'react';
+import {
+    AppBar,
+    Box,
+    Toolbar,
+    Typography,
+    TextField,
+    Container,
+    Button,
+    Avatar,
+    IconButton,
+    Paper,
+} from '@mui/material';
 import LockIcon from '@mui/icons-material/Lock';
 import LoginIcon from '@mui/icons-material/Login';
 import LogoutIcon from '@mui/icons-material/Logout';
 import AccountCircle from '@mui/icons-material/AccountCircle';
 import { Auth, getUser } from './auth';
-import { getUserFragments } from './api'
+import { getUserFragments, postFragment } from './api';
 
 function App() {
+    const [text, setText] = useState('');
+
     async function init() {
+        // eslint-disable-next-line react-hooks/rules-of-hooks
         const userSection = document.querySelector('#user');
         const loginBtn = document.querySelector('#login');
         const logoutBtn = document.querySelector('#logout');
@@ -34,6 +41,7 @@ function App() {
             return;
         }
         console.log({ user });
+
         userSection.hidden = false;
         userSection.innerText = user.username;
         loginBtn.disabled = true;
@@ -41,9 +49,22 @@ function App() {
         getUserFragments(user);
     }
 
+    async function submit(e) {
+        e.preventDefault();
+
+        const user = await getUser();
+        if (!user) {
+            return;
+        }
+        
+        await postFragment(user, text);
+        setText('');
+    }
+
     React.useEffect(() => {
         init();
     }, []);
+
     return (
         <>
             <Box sx={{ flexGrow: 1 }}>
@@ -126,6 +147,49 @@ function App() {
                             <LogoutIcon />
                             &nbsp;&nbsp;Log Out
                         </Button>
+                    </Box>
+                </Container>
+
+                <Container maxWidth='sm'>
+                    <Box
+                        sx={{
+                            marginTop: 25,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            width: 600,
+                            height: 300,
+                        }}>
+                        <Paper style={{ padding: 18 }}>
+                            <Typography
+                                variant='h5'
+                                align='center'
+                                component='h3'
+                                gutterBottom>
+                                Submit a Text to Fragment Server
+                            </Typography>
+                            <TextField
+                                id='fragment-text'
+                                name='text-box'
+                                type='text'
+                                value={text}
+                                placeholder='Enter a text to submit'
+                                fullWidth
+                                multiline
+                                rows={4}
+                                onChange={(e) => setText(e.target.value)}
+                            />
+                            <Button
+                                id='submit'
+                                type='submit'
+                                fullWidth
+                                variant='outlined'
+                                sx={{ mt: 3, mb: 2 }}
+                                onClick={submit}>
+                                &nbsp;&nbsp;&nbsp;&nbsp;Submit
+                                &nbsp;&nbsp;&nbsp;&nbsp;
+                            </Button>
+                        </Paper>
                     </Box>
                 </Container>
             </Box>
--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:8000';
 
 export async function getUserFragments(user) {
-    console.log('Requesting user fragments data...');
+    console.log('Requesting user fragments data...', apiUrl);
     try {
-        const res = await fetch(`${apiUrl}/v1/fragments`, {
+        const res = await fetch(`${apiUrl}v1/fragments`, {
             headers: user.authorizationHeaders(),
         });
+
         if (!res.ok) {
             throw new Error(`${res.status} ${res.statusText}`);
         }
@@ -13,5 +14,28 @@ export async function getUserFragments(user) {
         console.log('Got user fragments data', { data });
     } catch (err) {
         console.error('Unable to call GET /v1/fragments', { err });
+    }
+}
+
+export async function postFragment(user, text) {
+    try {
+        console.log('user idToken', user.idToken)
+        const headers = { 'Content-Type': 'text/plain'}
+        headers['Authorization'] = `Bearer ${user.idToken}`
+
+        const res = await fetch(`${apiUrl}v1/fragments`, {
+            headers: headers,
+            method: 'POST',
+            crossDomain: true,
+            body: text
+        });
+
+        if (!res.ok) {
+            throw new Error(`${res.status} ${res.statusText}`);
+        }
+        const data = await res.json();
+        console.log('Fragment has been posted', { data });
+    } catch (err) {
+        console.error('Unable to call POST /v1/fragments', { err });
     }
 }
